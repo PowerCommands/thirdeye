28f9a8dbbba42a42b3bdef7f7bb00f2b76d49dbc
Harri Klingsten <harri.klingsten2@scb.se>
2025-03-08 23:50:08 +0100

    Added a QuietConsoleWriter

---
+++ src/Agent/PainKiller.ThirdEyeAgent/Core/PainKiller.PowerCommands.Shared/Utils/QuietConsoleWriter.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Logging;
+using PainKiller.PowerCommands.Shared.Contracts;
+
+namespace PainKiller.PowerCommands.Shared.Utils;
+
+/// <summary>
+/// Does not write anything to the console, just writes to the log file.
+/// </summary>
+public class QuietConsoleWriter(ILogger log) : IConsoleWriter
+{
+    public void WriteLine(string output) => log.LogInformation($"{output}");
+    public void WriteHeadLine(string output) => log.LogInformation($"{output}");
+    public void Write(string output, ConsoleColor? color = null) => log.LogInformation($"{output}");
+    public void WriteSuccess(string output) => log.LogInformation($"{output}");
+    public void WriteSuccessLine(string output) => log.LogInformation($"{output}");
+    public void WriteFailure(string output) => log.LogInformation($"{output}");
+    public void WriteFailureLine(string output) => log.LogInformation($"{output}");
+    public void WriteCodeExample(string commandName, string text) => log.LogInformation($"{commandName} {text}");
+}
