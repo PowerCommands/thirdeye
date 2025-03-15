8c9475ed50df75c42e4a1b6ffe2ac93c2a78d696
Harri Klingsten <harri.klingsten2@scb.se>
2025-03-15 13:36:25 +0100

    Added support to analyze from repos.

--- src/Agent/PainKiller.ThirdEyeAgent/PainKiller.ThirdEyeAgentCommands/Managers/Workflows/AnalyzeProjectWorkflow.cs
+++ src/Agent/PainKiller.ThirdEyeAgent/PainKiller.ThirdEyeAgentCommands/Managers/Workflows/AnalyzeProjectWorkflow.cs
@@ -5,7 +5,7 @@ using PainKiller.ThirdEyeAgentCommands.Services;
 
 namespace PainKiller.ThirdEyeAgentCommands.Managers.Workflows;
 
-public class AnalyzeProjectWorkflow(IConsoleWriter writer, PowerCommandsConfiguration configuration) : AnalyzeWorkflowBase(writer, configuration)
+public class AnalyzeProjectWorkflow(IConsoleWriter writer2, PowerCommandsConfiguration configuration) : AnalyzeWorkflowBase(writer2, configuration)
 {
     public override void Run(params string[] args)
     {
@@ -15,7 +15,7 @@ public class AnalyzeProjectWorkflow(IConsoleWriter writer, PowerCommandsConfigur
         UpdateInfoPanel();
         if (VulnerableComponents.Count == 0)
         {
-            writer.WriteSuccessLine($"No vulnerabilities found in {projectName}!");
+            writer2.WriteSuccessLine($"No vulnerabilities found in {projectName}!");
             return;
         }
         var component = ViewCveDetails(VulnerableComponents);
@@ -28,7 +28,7 @@ public class AnalyzeProjectWorkflow(IConsoleWriter writer, PowerCommandsConfigur
     public override List<ComponentCve> GetVulnerableComponents(Repository? repository, string projectName = "")
     {
         var filteredThirdPartyComponents = repository == null ? ObjectStorageService.Service.GetThirdPartyComponents() : FilterService.Service.GetThirdPartyComponents(repository, projectName).ToList();
-        var analyzer = new CveAnalyzeManager(writer);
+        var analyzer = new CveAnalyzeManager(writer2);
         var threshold = ToolbarService.NavigateToolbar<CvssSeverity>();
         var components = analyzer.GetVulnerabilities(CveStorageService.Service.GetCveEntries(), filteredThirdPartyComponents, threshold);
         VulnerableComponents = PresentationManager.DisplayVulnerableComponents(components);
---
+++ src/Agent/PainKiller.ThirdEyeAgent/PainKiller.ThirdEyeAgentCommands/Managers/Workflows/AnalyzeRepositoryWorkflow.cs
@@ -0,0 +1,28 @@
+﻿using PainKiller.ThirdEyeAgentCommands.BaseClasses;
+using PainKiller.ThirdEyeAgentCommands.DomainObjects;
+using PainKiller.ThirdEyeAgentCommands.Services;
+
+namespace PainKiller.ThirdEyeAgentCommands.Managers.Workflows;
+
+public class AnalyzeRepositoryWorkflow(IConsoleWriter writer, PowerCommandsConfiguration configuration) : AnalyzeWorkflowBase(writer, configuration)
+{
+    public override void Run(params string[] args)
+    {
+        var repoName = args.First();
+        var repo = ObjectStorageService.Service.GetRepositories().First(r => r.Name == repoName);
+        Load();
+        GetVulnerableComponents(repo);
+        UpdateInfoPanel();
+        if (VulnerableComponents.Count == 0)
+        {
+            writer.WriteSuccessLine($"No vulnerabilities found in {repoName}!");
+            return;
+        }
+        var component = ViewCveDetails(VulnerableComponents);
+        if (component != null)
+        {
+            var viewWorkspace = DialogService.YesNoDialog("Do you want to view where the vulnerable components is used?");
+            if(viewWorkspace) WorkspaceSearch(new ThirdPartyComponent{Name = component.Name, Version = component.Version}, true);
+        }
+    }
+}
